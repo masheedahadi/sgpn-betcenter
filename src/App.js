ea2c11c865d8a495a30522e42afcf19da267c6ab
Masheed Ahadi <masheed@tallysight.com>
2022-06-27 14:28:41 -0700

    updated widgets for MLB

--- src/App.js
+++ src/App.js
@@ -46,10 +46,11 @@ class App extends Component {
                 <Image src={vbanner} width="100%" />
               </Col>
               <Col xs={12} md={6}>
-                <iframe src="https://tallysight.com/widget/analyst/munaf-manji/NBA/latest/tile:1?id=1d929c37-361c-484a-9ef6-f5e74d45759f" title="Munaf Manji Latest NBA Picks" height="600px" width="100%" scrolling="auto" style={{ border: 0, margin: 0 }}></iframe>
+                <iframe src="https://tallysight.com/widget/analyst/dylan-rockford/MLB/latest?id=3dabe373-4e27-45d3-aba3-4bef5b614347" title="Dylan Rockford MLB Picks" allowtransparency="true" height="600px" width="100%" scrolling="auto" style={{ border: 0, margin: 0 }}></iframe>
               </Col>
               <Col xs={12} md={3}>
-                <iframe src="https://tallysight.com/widget/tile/NBA/org:sports-gambling-podcast-network/event:2021-22-nba-futures/topic:nba-champion-1/variant:1/sportsbook:WynnBet?id=db04f510-8db4-42d1-a9ab-b07f7cadbb2c" title="Nick Dant NBA Picks" height="500px" width="100%" scrolling="auto" style={{ border: 0, margin: 0 }}></iframe>
+                <iframe src="https://tallysight.com/widget/tile/MLB/org:sports-gambling-podcast-network/event:mlb-futures-2022/topic:world-series-winner/variant:2?id=610fcdaf-80ba-49cf-81e9-7b38604f6a4b" title="MLB Winner" allowtransparency="true" height="500px" width="100%" scrolling="auto" style={{ border: 0, margin: 0 }}></iframe>
+                <iframe src="https://tallysight.com/widget/tile/MLB/org:sports-gambling-podcast-network/event:mlb-futures-2022/topic:home-run-crown/variant:2?id=dc35fdd1-f5a7-4d76-b7be-1f96d44cc368" title="Home Run Crown" allowtransparency="true" height="500px" width="100%" scrolling="auto" style={{ border: 0, margin: 0 }}></iframe>                
                 <iframe src="https://tallysight.com/widget/offers/sports-gambling-podcast-network/sportsbooks:wynnbet?id=db04f510-8db4-42d1-a9ab-b07f7cadbb2c" title="WynnBet Promos" height="150px" width="100%" scrolling="auto" style={{ border: 0, margin: 0 }}></iframe>
               </Col>
             </Row>
