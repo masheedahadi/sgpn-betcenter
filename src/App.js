28bdfe80fafc429069668248b073d584425c0927
Masheed Ahadi <masheed@tallysight.com>
2022-06-27 15:01:47 -0700

    removed unused code

--- src/App.js
+++ src/App.js
@@ -5,36 +5,6 @@ import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Image, Row, Col, Container } from 'react-bootstrap'
 
-// eslint-disable-next-line no-unused-vars
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
